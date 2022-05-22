7cf7125289f9422c44beecf2f4651af554ce2883
jesseryy <jesse.ryynala@gmail.com>
2022-05-22 23:09:43 +0300

    scriptien lisäystä ja korjauksia sivuihin

---
+++ scripts/malamuutti.js
@@ -0,0 +1,9 @@
+fetch('https://random.dog/woof.json')
+    .then(response => response.json())
+    .then(
+        data => {
+            document.getElementById('print-here').innerHTML = "<h2>" + data.answer + "</h2>";
+            document.getElementById('print-here').innerHTML += "<img src=\"" + data.image + "\"/>";
+            document.getElementById('print-here').innerHTML += '<img src="' + data.image + '">';
+        }
+    );
